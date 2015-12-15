e1c3cc67888961ad65d96db1ad125c446bd6aaa3
Miguel de la Cruz <mgdelacroix@gmail.com>
2015-12-15 15:55:10 +0100

    Integrate home template

--- src/baoqu/components/home.cljs
+++ src/baoqu/components/home.cljs
@@ -1,11 +1,5 @@
-(ns baoqu.components.home)
-
-(:require [rum.core :as rum])
-
-(rum/defc main < rum/reactive
-  "The main component for the home screen"
-  []
-  (header))
+(ns baoqu.components.home
+  (:require [rum.core :as rum]))
 
 (rum/defc header < rum/reactive
   []
@@ -13,48 +7,60 @@
    [:h1.logo "Baoqu"]
    [:div.event-name "¿Qué tipo de muerte dolorosa queremos para los ciclistas de La Guindalera?"]])
 
+(rum/defc footer < rum/static
+  []
+  [:div#mainFooter "user movidas"])
 
+(rum/defc ideas < rum/static
+  []
+  [:div.mod-ideas
+   [:div.mod-header
+    [:div.title "36 ideas"]
+    [:span.toggle
+     [:i {:class "fa fa-lg fa-chevron-right"}]]]
+   [:div.mod-body "ideas body"]
+   [:div.mod-add-box
+    [:input {:placeholder "Añade una nueva idea"}]
+    [:span.button
+     [:i {:class "fa fa-lg fa-plus"}]]]])
 
+(rum/defc comments < rum/static
+  []
+  [:div.mod-comments
+   [:div.mod-header
+    [:div.title "36 comments"]
+    [:span.toggle
+     [:i {:class "fa fa-lg fa-chevron-right"}]]]
+   [:div.mod-body "comments body"]
+   [:div.mod-add-box
+    [:input {:placeholder "Comenta"}]
+    [:span.button
+     [:i {:class "fa fa-lg fa-plus"}]]]])
 
+(rum/defc circle < rum/static
+  []
+  [:div.circle-wrapper
+   [:div.circle-header
+    [:div.circle-header-title "Círculo Onisuzume"]
+    [:span.circle-header-exit "Salir de este círculo"]]
+   [:div.circle-content
+    (ideas)
+    (comments)]])
 
+(rum/defc map < rum/reactive
+  []
+  [:div.map "mapa"])
 
+(rum/defc container < rum/reactive
+  []
+  [:div.container
+   (map)
+   (circle)])
 
-;;         <!-- CONTAINER-->
-;;         <div class="container">
-;;           <!-- MAPA-->
-;;           <div class="map">mapa</div>
-;;           <!--CIRCLE-->
-;;           <div class="circle-wrapper">
-;;             <div class="circle-header">
-;;               <div class="circle-header-title">Círculo Onisuzume</div><span class="circle-header-exit">Salir de este círculo</span>
-;;             </div>
-;;             <div class="circle-content">
-;;               <!-- IDEAS-->
-;;               <div class="mod-ideas">
-;;                 <div class="mod-header">
-;;                   <div class="title">36 ideas</div><span class="toggle"><i class="fa fa-lg fa-chevron-right"></i></span>
-;;                 </div>
-;;                 <div class="mod-body">ideas body</div>
-;;                 <div class="mod-add-box">
-;;                   <input placeholder="Añade una nueva idea"/><span class="button"><i class="fa fa-lg fa-plus"></i></span>
-;;                 </div>
-;;               </div>
-;;               <!-- COMMENTS-->
-;;               <div class="mod-comments">
-;;                 <div class="mod-header">
-;;                   <div class="title">36 comments</div><span class="toggle"><i class="fa fa-lg fa-chevron-right"></i></span>
-;;                 </div>
-;;                 <div class="mod-body">comments body</div>
-;;                 <div class="mod-add-box">
-;;                   <input placeholder="Comenta"/><span class="button"><i class="fa fa-lg fa-plus"></i></span>
-;;                 </div>
-;;               </div>
-;;             </div>
-;;           </div>
-;;         </div>
-;;         <!-- FOOTER-->
-;;         <div id="mainFooter">user movidas</div>
-;;       </div>
-;;     </body>
-;;   </head>
-;; </html>
+(rum/defc main < rum/reactive
+  "The main component for the home screen"
+  []
+  [:div.page
+   (header)
+   (container)
+   (footer)])
