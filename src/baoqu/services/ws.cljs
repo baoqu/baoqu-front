5c03ac6a03246e983bf61cc63146263ba07147fe
Miguel de la Cruz <mgdelacroix@gmail.com>
2015-12-17 11:54:40 +0100

    Multimethod added to ws messages

--- src/baoqu/services/ws.cljs
+++ src/baoqu/services/ws.cljs
@@ -12,6 +12,10 @@
   (let [r (t/reader :json {:handlers {"u" ->UUID}})]
     (t/read r data)))
 
+(defn response->message
+  [res]
+  (decode (.-message res)))
+
 (defn encode
   [data]
   (let [w (t/writer :json)]
@@ -35,15 +39,31 @@
   [res]
   (println "--> TICK: " res))
 
+(defmulti process-message
+  (fn [message]
+    (:topic message)))
+
+(defmethod process-message :events/create-circle
+  [message]
+  (js/alert message))
+
+(defmethod process-message :events/add-participant
+  [message]
+  (js/alert message))
+
+(defmethod process-message :events/status
+  [message]
+  (js/alert message))
+
+(def process-decoded-message (comp process-message response->message))
+
 (defn create-ws
   []
   (let [ws (WebSocket. false)
         uri "ws://127.0.0.1:5050/ws"]
-
-    (events/listen ws EventType.MESSAGE process-message)
+    (events/listen ws EventType.MESSAGE process-decoded-message)
     (events/listen ws EventType.ERROR process-error)
     (events/listen ws EventType.CLOSED process-closed)
     (events/listen ws EventType.TICK process-tick)
-
     (.open ws uri)
     ws))
