4685e29d978a8bcb871d9dbac45be8508e16295d
andy <andres.gonzalez@kaleidos.net>
2015-12-18 11:04:56 +0100

    notifications

--- src/baoqu/components/home.cljs
+++ src/baoqu/components/home.cljs
@@ -13,11 +13,29 @@
 (rum/defc notifications < rum/reactive
   []
   (let [state (rum/react d/state)
-  active-section (:active-section state)]
+  active-section (:active-section state)
+  active-notification (:active-notification state)]
   [:div.notifications
-    [:div.notification.info "This is a notification of something not that important"]
-    [:div.notification.success "This is a notification of success"]
-    [:div.notification.error "This is a notification of error"]
+    [:div.notification.success {:class (if (= active-notification "success") "active" "")} "Yay!"
+      [:div.fa.fa-close]
+    ]
+    [:div.notification.error {:class (if (= active-notification "error") "active" "")} "Nay!"
+      [:div.fa.fa-close]
+    ]
+    [:div.notification.info {:class (if (= active-notification "info") "active" "")} " ¯|_(ツ)_|¯ "
+      [:div.fa.fa-close]
+    ]
+    (letfn [(change-notification [notification] #(swap! d/state assoc :active-notification notification))]
+      [:div.test-notifications
+        [:div.fa.fa-send
+          [:ul
+            [:li {:on-click (change-notification "success")} "success"]
+            [:li {:on-click (change-notification "error")} "error"]
+            [:li {:on-click (change-notification "info")} "info"]
+          ]
+        ]
+      ]
+    )
   ]))
 
 
