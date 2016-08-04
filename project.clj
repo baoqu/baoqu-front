62e2669d194221b9497b1bb9b6aac9e606cc2440
Miguel de la Cruz <miguel@mcrx.me>
2016-08-04 12:58:25 +0200

    Add dist task

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                  [bidi "2.0.9"]
                  [funcool/httpurr "0.6.1"]
                  [com.cognitect/transit-cljs "0.8.232"]]
-  :plugins [[lein-figwheel "0.5.4-7"]]
+  :plugins [[lein-figwheel "0.5.4-7"]
+            [lein-cljsbuild "1.1.3"]]
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src/"]
                         :figwheel true
@@ -21,4 +22,5 @@
                                    :output-to "resources/public/js/main.js"
                                    :output-dir "resources/public/js"
                                    :verbose true}}]}
-  :figwheel {:css-dirs ["resources/public/css"]})
+  :figwheel {:css-dirs ["resources/public/css"]}
+  :aliases {"dist" ["cljsbuild" "once"]})
