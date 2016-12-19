a60685bd4487c4ca5f5efc1736ffd1c8c7b5aec4
Miguel de la Cruz <miguel@mcrx.me>
2016-12-19 15:52:38 +0100

    Add aux methods to utils

--- src/baoqu/utils.cljs
+++ src/baoqu/utils.cljs
@@ -1,4 +1,8 @@
-(ns baoqu.utils)
+(ns baoqu.utils
+  (:require [clojure.string :refer [join]]
+            [baoqu.config :refer [cfg]]))
+
+(enable-console-print!)
 
 (defn ->kwrds
   [m]
@@ -7,3 +11,18 @@
           (if (map? v)
             [(keyword k) (->kwrds v)]
             [(keyword k) v]))))
+
+(defn get-params-string
+  [params]
+  (join "&" (for [[k v] params]
+              (str (name k) "=" v))))
+
+(defn ->url
+  ([path]
+   (->url path {}))
+  ([path params]
+   (let [base-url (:server cfg)]
+     (if-not (empty? params)
+       (str base-url path)
+       (let [params-string (get-params-string params)]
+         (str base-url path "?" params-string))))))
