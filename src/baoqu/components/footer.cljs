dc6e45fab34bc6f11a520fbabcb57b0fa2b9cdaa
Miguel de la Cruz <miguel@mcrx.me>
2016-12-22 18:05:58 +0100

    Refactor footer and expose id to change color

--- src/baoqu/components/footer.cljs
+++ src/baoqu/components/footer.cljs
@@ -1,14 +1,14 @@
 (ns baoqu.components.footer
   (:require [rum.core :as rum]
             [baoqu.data :as d]
+            [baoqu.repos.user :as ur]
             [clojure.string :as s]))
 
 (rum/defc main < rum/reactive
   []
   (let [state (rum/react d/state)
-        username (get-in state [:me :name])
-        initial (s/upper-case (first username))]
-    ;; [:div#mainFooter "user movidas"]
+        {:keys [id name]} (ur/get-me)
+        initial (s/upper-case (first name))]
     [:div#user
      [:div.avatar
-      [:div.thumb initial]]]))
+      [:div.thumb {:class (str "color-" id)} initial]]]))
