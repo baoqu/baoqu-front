d000814c4a10606a46b59613599cab2e1cf6d6df
andy <andres.gonzalez@kaleidos.net>
2017-10-16 17:19:57 +0200

    layouts register y password

--- src/baoqu/components/fakelogin.cljs
+++ src/baoqu/components/fakelogin.cljs
@@ -12,13 +12,46 @@
       [:div.logo-icon]
       [:h1.logo "Baoqu"]
     ]
+  ; start login
    [:div.login.login-general
     [:div.event-info
       [:h2 "Entra en Baoqu"]
       [:p ""]
     ]
     [:form.input-box
-     [:input {:class "bt input-text" :placeholder "Nombre de usuario"}]
-     [:input {:class "bt input-text" :placeholder "Contraseña" :type "password"}]
+     [:input {:class "bt input-text" :placeholder "Nombre de usuario" :required ""}]
+     [:input {:class "bt input-text" :placeholder "Contraseña" :type "password" :required ""}]
      [:button "Entrar"]
-     [:a.bottom-link {:href "#register"} "¿No tienes cuenta? Regístrate"]]]])
+     [:a.bottom-link {:href "#register"} "¿No tienes cuenta? Regístrate"]
+    ]]
+    ; end login
+
+    ; start registro
+     [:div.login.login-general
+      [:div.event-info
+        [:h2 "Regístrate en Baoqu"]
+        [:p ""]
+      ]
+      [:form.input-box
+       [:input {:class "bt input-text" :placeholder "Nombre de usuario" :required ""}]
+       [:input {:class "bt input-text" :placeholder "Email" :type "email" :required ""}]
+       [:button "Registrarse"]
+       [:a.bottom-link {:href "#login"} "¿Ya tienes cuenta? Log in"]
+       ]]
+       ; end registro
+
+     ; start reset
+      [:div.login.login-general
+       [:div.event-info
+         [:h2 "Crea tu contraseña"]
+         [:p ""]
+       ]
+       [:form.input-box
+        [:input {:class "bt input-text" :placeholder "Contraseña" :type "password" :required ""}]
+        [:input {:class "bt input-text" :placeholder "Repite la contraseña" :type "password" :required ""}]
+        [:button "Registrarse"]
+        [:a.bottom-link {:href "#login"} "¿Ya tienes cuenta? Log in"]
+        ]]
+        ; end reset
+
+     ])
